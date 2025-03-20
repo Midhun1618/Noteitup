2855986f55807b290e4df11c7e8526431a651693
midhun1618 <midhumidhun342@gmail.com>
2025-03-20 12:48:40 +0530

    fragment basics

--- app/src/main/java/com/voxcom/noteitup/MainActivity.kt
+++ app/src/main/java/com/voxcom/noteitup/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.voxcom.noteitup
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragmentChange: Button
+    private var currentFragmentIndex = 0
+    private val fragmentNames = arrayOf( "Sticky","To-Do", "Notify")
+    private val fragments = arrayOf(TodoFragment(), StickyFragment(), NotifyFragment())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        fragmentChange = findViewById(R.id.fragmentChange)
+        fragmentChange.text = fragmentNames[currentFragmentIndex]
 
+        // Load initial fragment
+        loadFragment(fragments[currentFragmentIndex])
 
+        fragmentChange.setOnClickListener {
+            currentFragmentIndex = (currentFragmentIndex + 1) % fragments.size
+            fragmentChange.text = fragmentNames[currentFragmentIndex]
+            loadFragment(fragments[currentFragmentIndex])
+        }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
 }
+
---
+++ app/src/main/java/com/voxcom/noteitup/TodoFragment.kt
@@ -0,0 +1,16 @@
+package com.voxcom.noteitup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TodoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+}
